d03ac0ac25dc17de3c2fe2a568b8620e2e6d24a8
Ping Yin <pkufranky@gmail.com>
2010-11-13 15:42:50 +0800

    XPathSelectorList: add method extract1 and re1

--- scrapy/selector/list.py
+++ scrapy/selector/list.py
@@ -12,8 +12,26 @@ class XPathSelectorList(list):
     def re(self, regex):
         return flatten([x.re(regex) for x in self])
 
+    def re1(self, regex, index=0):
+        """Perform the re() method on each XPathSelector of the list, and
+        return the result as a flattened list of unicode strings"""
+        matches = self.re(regex)
+        if len(matches) > index:
+            return matches[index]
+        else:
+            return None
+
     def extract(self):
         return [x.extract() for x in self]
+
+    def extract1(self, index=0):
+        """Return a unicode string of the content referenced by the first
+        XPathSelector of the list, or None if the list is empty"""
+        results = self.extract()
+        try:
+            return results[index]
+        except IndexError:
+            return None
 
     def extract_unquoted(self):
         return [x.extract_unquoted() for x in self]
